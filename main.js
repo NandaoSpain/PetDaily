c2b889e1326cb8bff5f6455baa2df2c6ddecf9df
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2024-11-30 08:48:34 -0300

    css

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Seleciona os elementos necessários
 const openModalBtn = document.getElementById('openModalBtn')
 const closeModalBtn = document.getElementById('closeModalBtn')
+const closeModalSpan = document.getElementById('closeModalSpan')
 const modal = document.getElementById('modal')
 
 // Abre o modal
@@ -12,7 +13,9 @@ openModalBtn.addEventListener('click', () => {
 closeModalBtn.addEventListener('click', () => {
     modal.style.display = 'none' // Esconde o modal
 })
-
+closeModalSpan.addEventListener('click', () => {
+    modal.style.display = 'none' // Esconde o modal
+})
 // Fecha o modal ao clicar fora do conteúdo
 window.addEventListener('click', (event) => {
     if (event.target === modal) {
