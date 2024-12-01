dd9623fc31ebe171f7483d514b79f1997e077548
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2024-12-01 12:35:34 -0300

    adjustment css

--- src/utils/submit.js
+++ src/utils/submit.js
@@ -1,10 +1,11 @@
-const dayjs = require("dayjs");
+import dayjs from "dayjs"
 const form = document.querySelector('form')
-const selectedDate = document.getElementById('date')
+const selectedDate = document.getElementById('date-time')
+const showDate = document.getElementById('date')
 
 // Carrega a data atual
 selectedDate.value = dayjs(new Date()).format('YYYY-MM-DD')
-
+showDate.value = dayjs(new Date()).format('YYYY-MM-DD')
 // Define a data minima como sendo o dia atual
 selectedDate.min = dayjs(new Date()).format('YYYY-MM-DD')
 
