41f7f51cd32394a629fd0b03de2f57c69cb8b96c
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2024-12-03 12:38:57 -0300

    isFuture

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,8 +1,10 @@
 import dayjs from "dayjs"
 import { openingHours } from "../../utils/openning-hours.js"
+const hours = document.getElementById("time-date")
+const inputDate = document.getElementById("date-time")
 export function hoursLoad({ date }) {
   // Limpa a lista de horários
-  hour.innerHTML = ''
+  hours.innerHTML = ''
   /* Aqui fiz uma requisição para o arquivo de horas abertas, fiz um map para exibir
   as horas de abertura fazendo um split e para separar pelos 2 pontos, desestruturando
   e pegando somente a primeira posição do array, omitindo a segunda posição.*/
@@ -12,16 +14,20 @@ export function hoursLoad({ date }) {
   // Verifica se a hora agora é anterior à hora de abertura do horário de atendimento.
   const isHourPast = dayjs(date).add(scheduleHour, "hour").isBefore(dayjs())
   
+  
   // retorna se o horário está disponivel
   return {
     hour,
-    isAvailable:!isHourPast,
+    isAvailable: !isHourPast 
   }
 })
+const isFuture = date < inputDate.value
+
 const select = document.querySelector("select")
 // Cria um select com os horários de atendimento disponíveis, já preenchendo com a hora atual.
 opening.forEach(({ hour, isAvailable}) =>  {  
   const option = document.createElement("option")
+  if (isFuture) isAvailable = true
   if (isAvailable) {
     option.selected = true
     option.style.color = "#B7D4FF"
