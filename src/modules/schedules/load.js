a03341dd0b4be8ee7f58a1d1111e3c682e79280d
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2024-12-06 19:57:28 -0300

    dailySchedule

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,16 +1,18 @@
-import { hoursLoad } from "../form/hours-load";
-import { renderSchedules } from "./render-schedules";
+import { hoursLoad } from "../form/hours-load.js";
+import { renderSchedules } from "./render-schedules.js";
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
 
 // seleciona o input de data
 const selectedDate = document.getElementById("date")
 
-export function schedulesDay() {
+export async function schedulesDay() {
   // obtém a data do input
   const date = selectedDate.value
   
-
+  const dailySchedule = await scheduleFetchByDay({ date })
+  console.log(dailySchedule)
   // Renderiza as horas disponíveis.
   hoursLoad({ date })
-  renderSchedules({ date})
+  renderSchedules({ date })
 
 }
--- src/services/schedule-fetch-by-day.js
+++ src/services/schedule-fetch-by-day.js
@@ -2,9 +2,11 @@ import dayjs from "dayjs"
 import { apiConfig } from "./api-config"
 export async function scheduleFetchByDay({ date }) {
   try {
+    // aqui vai fazer a chamada para o backend usando fetch
     const response = await fetch (`${apiConfig.baseUrl}/schedules`)
+    // converte a resposta para json
     const data = await response.json()
-
+    //filtra os agendamentos pelo dia selecionado
     const dailySchedules = data.filter((schedule) => 
       dayjs(date).isSame(schedule.when, "day")
     )
